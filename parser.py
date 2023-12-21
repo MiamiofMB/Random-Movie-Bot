9f52c10a8927d0fbbdf625813adb8100c83fa2d9
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2023-12-21 12:09:44 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,13 +1,14 @@
 import json
 import requests
 from random import *
-from secret.py import load_keys
+from secret import Load
 
-load_keys()
+
 
 headers={
-    'X-API-KEY':header
+    'X-API-KEY':Load.head()
 }
+
 def rand_mov(ganre):
     page = randint(1,200)
     r= requests.get(f'https://api.kinopoisk.dev/v1.3/movie?page={page}&genres.name={ganre}',headers=headers)
